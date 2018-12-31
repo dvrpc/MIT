b7f12a283d3a980c661829678e1e69962516a8cc
Marc Molta <mcmolta10@gmail.com>
2018-12-31 10:57:40 -0500

    Full Toolkit structure set up

--- js/fullToolkit.js
+++ js/fullToolkit.js
@@ -1,28 +1,28 @@
-// get a handle on all the necessary elements
-const principlesList = document.getElementById('principles-list')
+// doing all of the HTML manually because there isn't consistency w/the names or layout for the full toolkit table and the tools endpoint
+    // so this file is just accordion stuff
+const accordions = document.querySelectorAll('.accordion') 
 
-const options = {
-    method: 'GET',
-    mode: 'cors',
-    headers: {
-        'Content-Type': 'application/json; charset=utf-8',
+for(var i = 0; i < accordions.length; i++){
+    accordions[i].onclick = function(){
+        // show/hide the accordions on click
+        this.classList.toggle('active')
+
+        // toggle the aria-expanded attribute of the accordion button
+        let ariaExpandedBool = this.getAttribute('aria-expanded')
+        ariaExpandedBool === 'false' ? ariaExpandedBool = 'true' : ariaExpandedBool = 'false'
+        this.setAttribute('aria-expanded', ariaExpandedBool)
+
+        // toggle the aria-hidden attribute of the accordion panel
+        const panel = this.nextElementSibling
+        let ariaHiddenBool = panel.getAttribute('aria-hidden')
+        ariaHiddenBool === 'false' ? ariaHiddenBool = 'true' : ariaHiddenBool = 'false'
+        panel.setAttribute('aria-hidden', ariaHiddenBool)
+
+        // show/hide the panel on click
+        if(panel.style.maxHeight){
+            panel.style.maxHeight = null
+        }else{
+            panel.style.maxHeight = panel.scrollHeight + 'px'
+        }
     }
-}
-
-// fetch all statements?
-getPrinciples = (async () => {
-    const stream = await fetch('https://alpha.dvrpc.org/mitoolbox/word/', options)
-    const response = await stream.json()
-
-    const fragment = document.createDocumentFragment()
-    response.forEach(principle => addPrinciple(principle, fragment))
-
-    principlesList.appendChild(fragment)
-})()
-
-addPrinciple = (principle, fragment) => {
-    const li = document.createElement('li')
-    li.classList.add('principle')
-    li.textContent = principle.name
-    fragment.appendChild(li)
 }
